b87e2cb73d5ec8c997b3e98813b2c1eadaa9ac69
Vladimir <urkytsk3@gmail.com>
2023-06-23 17:36:55 +0800

    Базовый класс модели представления

---
+++ ViewModels/BaseView/BaseViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinkoffPriceMonitor.ViewModels.BaseView
+{
+    internal class BaseViewModel : INotifyPropertyChanged, IDisposable
+    {
+        public virtual event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnProperty([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        // Метод установки свойст
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnProperty(PropertyName);
+
+            return true;
+
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        private bool _disposed;
+
+        protected virtual void Dispose(bool Disposing)
+        {
+            if (!Disposing || _disposed) return;
+            _disposed = true;
+        }
+
+    }
+}
