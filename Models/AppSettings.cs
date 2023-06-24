b2578f4698ff13fc272f634d0a44f90912d55083
Vladimir <urkytsk3@gmail.com>
2023-06-24 16:03:07 +0800

    Рефактор AppSettings

--- Models/AppSettings.cs
+++ Models/AppSettings.cs
@@ -6,18 +6,16 @@ using System.Threading.Tasks;
 
 namespace TinkoffPriceMonitor.Models
 {
+    public class TickerGroup
+    {
+        public List<string> Tickers { get; set; }
+        public double PercentageThreshold { get; set; }
+        public int Interval { get; set; }
+    }
+
     public class AppSettings
     {
-        public string? TickerGroup1 { get; set; }
-        public double PercentageThresholdGroup1 { get; set; }
-        public int IntervalGroup1 { get; set; }
-
-        public string? TickerGroup2 { get; set; }
-        public double PercentageThresholdGroup2 { get; set; }
-        public int IntervalGroup2 { get; set; }
-
-        public string? TickerGroup3 { get; set; }
-        public double PercentageThresholdGroup3 { get; set; }
-        public int IntervalGroup3 { get; set; }
+        public List<TickerGroup> TickerGroups { get; set; }
     }
+
 }
