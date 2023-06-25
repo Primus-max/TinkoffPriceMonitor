73a3b9e585e0343fb219c73c552aef794e405067
Vladimir <urkytsk3@gmail.com>
2023-06-25 09:39:54 +0800

    Добавил отображение тикеров в ListView

--- Models/TrackedTickerInfo.cs
+++ Models/TrackedTickerInfo.cs
@@ -4,8 +4,8 @@ namespace TinkoffPriceMonitor.Models
 {
     public class TrackedTickerInfo
     {
-        public string GroupName { get; set; }
-        public string TickerName { get; set; }
+        public string? GroupName { get; set; }
+        public string? TickerName { get; set; }
         public decimal Price { get; set; }
         public decimal PriceChangePercentage { get; set; }
         public DateTime EventTime { get; set; }
