814027efd5d58b5e658b17789e8aa7697ed95992
Goga <urkytsk3@gmail.com>
2023-07-09 09:11:29 +0800

    Добавил кнопку сохранить(настройки)

---
+++ Models/SettingsModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinkoffPriceMonitor.Models
+{
+    public class SettingsModel
+    {
+        public string TinkoffToken { get; set; }
+        public string ChromeLocation { get; set; }
+    }
+}
